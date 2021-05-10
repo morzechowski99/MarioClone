1c9dc42be03648b5dc367ab9963ce07dd94e804f
Marcin Orzechowski <marcinorz@o2.pl>
2021-05-10 21:07:59 +0200

    nie dam rady wiecej :(

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+class Game {
+   constructor() {
+      this.lifes = 5;
+      this.mariobBig = false;
+      this.score = 0;
+      this.gameOver = false;
+   }
+
+   addScore = () => {
+      this.score++;
+   };
+
+   makeMarioBig = () => {
+      this.mariobBig = true;
+   };
+
+   addLife = () => {
+      this.lifes++;
+   };
+}
+
 const config = {
    type: Phaser.AUTO,
    width: 1000,
@@ -27,17 +48,14 @@ function preload() {
       "https://examples.phaser.io/assets/tilemaps/tiles/super_mario.png"
    );
 
-   // this.load.spritesheet(
-   //    "player",
-   //    "https://examples.phaser.io/assets/games/starstruck/dude.png",
-   //    {
-   //       frameWidth: 32,
-   //       frameHeight: 48,
-   //    }
-   // );
    this.load.spritesheet("player", "assets/marioSmall.png", {
       frameWidth: 34,
       frameHeight: 34,
+   });
+
+   this.load.spritesheet("enemy", "assets/enemy.png", {
+      frameWidth: 49,
+      frameHeight: 41,
    });
 }
 
@@ -53,8 +71,13 @@ let winText;
 let gameOver = false;
 const END_GAME = 7953;
 let reset;
+let gameInstance;
+let scoreText;
+let enemy;
 
 function create() {
+   gameInstance = new Game();
+
    map1 = this.make.tilemap({ key: "map1" });
    tileset1 = map1.addTilesetImage("SuperMarioBros-World1-1", "tiles1");
 
@@ -62,24 +85,16 @@ function create() {
 
    //kolizje
    map1.setCollision([
-      14,
-      15,
-      16,
-      20,
-      21,
-      22,
-      23,
-      24,
-      25,
-      27,
-      28,
-      29,
-      33,
-      39,
-      40,
+      14, 15, 16, 20, 21, 22, 23, 24, 25, 27, 28, 29, 33, 39, 40,
    ]);
 
+   //kolizja z coinem
    map1.setTileIndexCallback(11, getCoin, this);
+   //czerwony grzyb
+   map1.setTileIndexCallback(12, makeMarioBig, this);
+
+   //zielony grzyb
+   map1.setTileIndexCallback(18, addLife, this);
 
    const SCALE = game.scale.height / layer1.height;
 
@@ -88,9 +103,12 @@ function create() {
    cursors = this.input.keyboard.createCursorKeys();
 
    player = this.physics.add.sprite(50, 100, "player");
-   player.setCollideWorldBounds(true, true, true, false);
+   player.setCollideWorldBounds(true, true, false, false);
    player.setBounce(0.2);
-   player.body.gravity.y = 500;
+   player.body.gravity.y = 700;
+
+   enemy = this.physics.add.sprite(1000, 100, "enemy");
+   enemy.body.gravity.y = 500;
 
    //kamera
    this.cameras.main.setBounds(
@@ -110,7 +128,9 @@ function create() {
    //animacje
    this.anims.create({
       key: "left",
-      frames: this.anims.generateFrameNumbers("player", { frames: [2, 4, 5] }),
+      frames: this.anims.generateFrameNumbers("player", {
+         frames: [2, 4, 5],
+      }),
       frameRate: 10,
       repeat: -1,
    });
@@ -154,12 +174,27 @@ function create() {
    winText.setOrigin(0.5);
    winText.visible = false;
 
+   //score
+   scoreText = this.add.text(
+      16,
+      16,
+      `Score: ${gameInstance.score} \nLifes: ${gameInstance.lifes}`,
+      {
+         fontSize: "32px",
+         fill: "#000",
+      }
+   );
+   scoreText.setScrollFactor(0);
    reset = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 }
+
 let isJumping = false;
 
 function update() {
    this.physics.collide(player, layer1);
+   this.physics.collide(enemy, layer1);
+
+   if (enemy.body.onFloor()) enemy.setVelocityX(-100);
 
    if (!gameOver) {
       if (cursors.up.isDown) {
@@ -186,7 +221,7 @@ function update() {
          if (!isJumping) player.anims.play("front");
       }
 
-      if (player.y > game.scale.height - 20) {
+      if (player.y > game.scale.height - 40) {
          const screenCenterX =
             this.cameras.main.worldView.x + this.cameras.main.width / 2;
          const screenCenterY =
@@ -217,6 +252,30 @@ function update() {
       gameOver = false;
    }
 }
-function getCoin(player, coin) {
-   layer1.replaceByIndex(11, 1, coin.x, coin.y, 1, 1);
+function getCoin(sprite, coin) {
+   if (sprite === player) {
+      layer1.replaceByIndex(11, 1, coin.x, coin.y, 1, 1);
+      gameInstance.addScore();
+      scoreText.setText(
+         `Score: ${gameInstance.score} \nLifes: ${gameInstance.lifes}`
+      );
+   }
+}
+
+function makeMarioBig(sprite, mushroom) {
+   if (sprite === player) {
+      layer1.replaceByIndex(12, 1, mushroom.x, mushroom.y, 1, 1);
+      gameInstance.makeMarioBig();
+      player.scaleY = 2;
+   }
+}
+
+function addLife(sprite, mushroom) {
+   if (sprite === player) {
+      layer1.replaceByIndex(18, 1, mushroom.x, mushroom.y, 1, 1);
+      gameInstance.addLife();
+      scoreText.setText(
+         `Score: ${gameInstance.score} \nLifes: ${gameInstance.lifes}`
+      );
+   }
 }
